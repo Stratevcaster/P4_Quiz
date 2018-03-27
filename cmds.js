92cfcd296555b3311ad41fd7e1087c317cb346e6
Stratevcaster <yanistrate@yahoo.com>
2018-03-27 09:31:55 +0200

    Update cmds.js

--- cmds.js
+++ cmds.js
@@ -172,8 +172,8 @@ exports.deleteCmd = (socket,rl,id) => {
 };
 
 exports.creditsCmd = (socket,rl) => {
-    console.log(socket,'Autores de la practica');
-    console.log(socket,'YANI');
+    log(socket,'Autores de la practica');
+    log(socket,'YANI');
     rl.prompt();
 };
 
